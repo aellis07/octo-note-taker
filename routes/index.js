80e7d8e18e95133e5d911ca31c096dd3e11cf0e9
Anthony Ellis <amellis245@gmail.com>
2021-11-19 20:57:37 -0500

    added comments to index.js file

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ const path = require("path");
 const router = require("express").Router();
 const fs = require("fs");
 
+// GET Routes
+// Index/Starting page
 router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+// Notes page
 router.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
