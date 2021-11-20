88aa2d770aff187cca8703cecb09891f8651e409
Anthony Ellis <amellis245@gmail.com>
2021-11-19 20:15:43 -0500

    added route index dependencies

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,3 @@
 const path = require("path");
 const router = require("express").Router();
-
-router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-router.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-module.exports = router;
+const fs = require("fs");
