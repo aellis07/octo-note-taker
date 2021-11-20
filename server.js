a3d27b6dc6b4d74c28feb0ea7a11fcaf9069ab5d
Anthony Ellis <amellis245@gmail.com>
2021-11-19 20:19:38 -0500

    added server.js dependecies

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
 const express = require("express");
-const fs = require("fs");
 const htmlRoutes = require("./routes/index.js");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-app.use(htmlRoutes);
-
-// ==============================
-// SERVER LISTENER
-app.listen(PORT, () =>
-    //-----Console log left for convenience of local use
-    console.log(`App listening on PORT http://localhost:${PORT}`)
-);
+const PORT = 3000;
