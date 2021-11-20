9a61f84f1ca339f4b3b5475f6f929c87cd8b9f18
Anthony Ellis <amellis245@gmail.com>
2021-11-19 20:16:32 -0500

    added get routes for the main index.html page as well as the notes.html

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
 const path = require("path");
 const router = require("express").Router();
 const fs = require("fs");
+
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
