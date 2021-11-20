81ad51b8c74c6736b48460f3af33cd8ff0520c91
Anthony Ellis <amellis245@gmail.com>
2021-11-19 20:52:36 -0500

    added server.js middleware

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const htmlRoutes = require("./routes/index.js");
 const app = express();
 
 const PORT = 3000;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static frontend public folder
+app.use(express.static("public"));
+
+// Routes to both index.html and note.html
+app.use(htmlRoutes);
