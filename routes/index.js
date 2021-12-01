ab64bc76337727aa6aa59d2cffda9443e844ad20
Anthony Ellis <amellis245@gmail.com>
2021-12-01 12:16:17 -0500

    get route that gets notes stored in the database

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,29 @@ const noteData = require("../db/db.json");
 // GET Routes
 // Index/Starting page
 router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // Notes page
 router.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-//Saved notes on notes page
-router.get("/api/notes", (req, res) => res.json(noteData));
+//Saved notes from db
+router.get("/api/notes", (req, res) => {
+  fs.readFile(
+    "/Users/Tony/Desktop/bootcamp homework/octo-note-taker/db/db.json",
+    "utf8",
+    (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    }
+  );
+});
 
 // POST
 router.post("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 module.exports = router;
